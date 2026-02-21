c7329e191a3a836f457d8a7a19e17a49982c1383
Tomas <tomas.a.gallardo006@gmail.com>
2026-02-21 20:59:29 -0300

    DAO registro de tareas y busqueda hecho.

--- app/src/main/java/frgp/utn/edu/tpgrupo27/database/DAO/DaoTarea.java
+++ app/src/main/java/frgp/utn/edu/tpgrupo27/database/DAO/DaoTarea.java
@@ -1,4 +1,68 @@
 package frgp.utn.edu.tpgrupo27.database.DAO;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import frgp.utn.edu.tpgrupo27.database.BaseSQLite;
+import frgp.utn.edu.tpgrupo27.entidades.Tarea;
+
 public class DaoTarea {
+    private BaseSQLite baseDeDatos;
+
+    public DaoTarea (Context context){
+        baseDeDatos = new BaseSQLite(context,"baseDeDatosAPP",null,1);
+    }
+
+    public boolean altaTarea (Tarea tarea){
+        if (tarea.getNombreTarea() != null && !tarea.getNombreTarea().isEmpty() &&
+                tarea.getDescripcionTarea() != null && !tarea.getDescripcionTarea().isEmpty() &&
+                tarea.getFechaInicio() > 0 &&
+                tarea.getFechaFinal() > 0 &&
+                tarea.getPrioridad() > 0){
+            return false;
+        }
+        SQLiteDatabase baseDatosApp = baseDeDatos.getWritableDatabase();
+
+        ContentValues registrar = new ContentValues();
+        registrar.put("nombreTarea", tarea.getNombreTarea());
+        registrar.put("descripcionTarea", tarea.getDescripcionTarea());
+        registrar.put("fechaInicio", tarea.getFechaInicio());
+        registrar.put("fechaFinal", tarea.getFechaFinal());
+        registrar.put("prioridad", tarea.getPrioridad());
+
+        baseDatosApp.insert("tarea", null, registrar);
+
+        baseDatosApp.close();
+        return true;
+    }
+
+    public boolean buscarTarea (Tarea tarea){
+        SQLiteDatabase baseDatosApp = baseDeDatos.getWritableDatabase();
+        if(tarea.getNombreTarea()!= null){
+            Cursor fila = baseDatosApp.rawQuery(
+                    "SELECT nombreTarea, descripcionTarea, fechaInicio, fechaFinal, prioridad " +
+                            "FROM tarea WHERE nombreTarea = "+
+                    tarea.getNombreTarea(), null
+            );
+            if (fila.moveToFirst()){
+                tarea.setNombreTarea(fila.getString(0));
+                tarea.setDescripcionTarea(fila.getString(1));
+                tarea.setFechaInicio(fila.getLong(2));
+                tarea.setFechaFinal(fila.getLong(3));
+                tarea.setPrioridad(fila.getInt(4));
+                baseDatosApp.close();
+                return true;
+            }else {
+                return false;
+            }
+
+
+
+        }
+
+
+        return false;
+    }
 }
--- app/src/main/java/frgp/utn/edu/tpgrupo27/entidades/Tarea.java
+++ app/src/main/java/frgp/utn/edu/tpgrupo27/entidades/Tarea.java
@@ -1,4 +1,73 @@
 package frgp.utn.edu.tpgrupo27.entidades;
 
 public class Tarea {
+  private int idTarea;
+  private String nombreTarea;
+  private String descripcionTarea;
+
+  private long fechaInicio;
+  private long fechaFinal;
+
+  private int prioridad;
+
+  public Tarea () {
+
+  }
+
+  public Tarea (String nombreTarea, String descripcionTarea, long fechaInicio,
+                long fechaFinal, int prioridad) {
+    this.nombreTarea = nombreTarea;
+    this.descripcionTarea = descripcionTarea;
+    this.fechaInicio = fechaInicio;
+    this.fechaFinal = fechaFinal;
+    this.prioridad = prioridad;
+  }
+
+    public int getIdTarea() {
+        return idTarea;
+    }
+
+    public void setIdTarea(int idTarea) {
+        this.idTarea = idTarea;
+    }
+
+    public String getNombreTarea() {
+        return nombreTarea;
+    }
+
+    public void setNombreTarea(String nombreTarea) {
+        this.nombreTarea = nombreTarea;
+    }
+
+    public String getDescripcionTarea() {
+        return descripcionTarea;
+    }
+
+    public void setDescripcionTarea(String descripcionTarea) {
+        this.descripcionTarea = descripcionTarea;
+    }
+
+    public long getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(long fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public long getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(long fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
 }
