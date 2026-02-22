bf2c4efe2acfafbb5067bfb0eacdeb70957cf74e
Tomas <tomas.a.gallardo006@gmail.com>
2026-02-22 15:15:09 -0300

    CRUD en el DAO de tareas hecho.

--- app/src/main/java/frgp/utn/edu/tpgrupo27/database/DAO/DaoTarea.java
+++ app/src/main/java/frgp/utn/edu/tpgrupo27/database/DAO/DaoTarea.java
@@ -21,21 +21,23 @@ public class DaoTarea {
                 tarea.getFechaInicio() > 0 &&
                 tarea.getFechaFinal() > 0 &&
                 tarea.getPrioridad() > 0){
+            SQLiteDatabase baseDatosApp = baseDeDatos.getWritableDatabase();
+
+            ContentValues registrar = new ContentValues();
+            registrar.put("nombreTarea", tarea.getNombreTarea());
+            registrar.put("descripcionTarea", tarea.getDescripcionTarea());
+            registrar.put("fechaInicio", tarea.getFechaInicio());
+            registrar.put("fechaFinal", tarea.getFechaFinal());
+            registrar.put("prioridad", tarea.getPrioridad());
+
+            baseDatosApp.insert("tarea", null, registrar);
+
+            baseDatosApp.close();
+            return true;
+        } else{
             return false;
         }
-        SQLiteDatabase baseDatosApp = baseDeDatos.getWritableDatabase();
 
-        ContentValues registrar = new ContentValues();
-        registrar.put("nombreTarea", tarea.getNombreTarea());
-        registrar.put("descripcionTarea", tarea.getDescripcionTarea());
-        registrar.put("fechaInicio", tarea.getFechaInicio());
-        registrar.put("fechaFinal", tarea.getFechaFinal());
-        registrar.put("prioridad", tarea.getPrioridad());
-
-        baseDatosApp.insert("tarea", null, registrar);
-
-        baseDatosApp.close();
-        return true;
     }
 
     public boolean buscarTarea (Tarea tarea){
@@ -47,11 +49,12 @@ public class DaoTarea {
                     tarea.getNombreTarea(), null
             );
             if (fila.moveToFirst()){
-                tarea.setNombreTarea(fila.getString(0));
-                tarea.setDescripcionTarea(fila.getString(1));
-                tarea.setFechaInicio(fila.getLong(2));
-                tarea.setFechaFinal(fila.getLong(3));
-                tarea.setPrioridad(fila.getInt(4));
+                tarea.setDescripcionTarea(fila.getString(0));
+                tarea.setFechaInicio(fila.getLong(1));
+                tarea.setFechaFinal(fila.getLong(2));
+                tarea.setPrioridad(fila.getInt(3));
+
+                fila.close();
                 baseDatosApp.close();
                 return true;
             }else {
@@ -59,5 +62,52 @@ public class DaoTarea {
             }
         }
         return false;
+    }
+
+    public boolean modificarTarea (Tarea tarea) {
+        if (tarea.getNombreTarea() != null && !tarea.getNombreTarea().isEmpty() &&
+                tarea.getDescripcionTarea() != null && !tarea.getDescripcionTarea().isEmpty() &&
+                tarea.getFechaInicio() > 0 &&
+                tarea.getFechaFinal() > 0 &&
+                tarea.getPrioridad() > 0){
+            SQLiteDatabase baseDatosApp = baseDeDatos.getWritableDatabase();
+
+            ContentValues registrar = new ContentValues();
+            registrar.put("nombreTarea", tarea.getNombreTarea());
+            registrar.put("descripcionTarea", tarea.getDescripcionTarea());
+            registrar.put("fechaInicio", tarea.getFechaInicio());
+            registrar.put("fechaFinal", tarea.getFechaFinal());
+            registrar.put("prioridad", tarea.getPrioridad());
+
+            int cantidad = baseDatosApp.update
+                    ("tarea",registrar,"where nombreTarea ="+
+                            tarea.getNombreTarea(),null);
+            baseDatosApp.close();
+            if (cantidad > 0){
+                return true;
+            }else{
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean bajaTarea (Tarea tarea){
+        SQLiteDatabase baseDatosApp = baseDeDatos.getWritableDatabase();
+        String nombreT = tarea.getNombreTarea();
+        if(!nombreT.isEmpty()){
+            int cantidad = baseDatosApp.delete
+                    ("tarea","where nombreTarea=" + nombreT, null);
+            if (cantidad == 1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
     }
 }
