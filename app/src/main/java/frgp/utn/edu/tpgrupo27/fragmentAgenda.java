74e5b92ea6fe8c91e96aa6e32b5c2721a11f7145
FEDE <federico.pierdominici@gmail.com>
2026-03-21 18:05:09 -0300

    filtro con spinner

--- app/src/main/java/frgp/utn/edu/tpgrupo27/fragmentAgenda.java
+++ app/src/main/java/frgp/utn/edu/tpgrupo27/fragmentAgenda.java
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +23,11 @@ import frgp.utn.edu.tpgrupo27.negocio.negocioTarea;
 import utils.session;
 
 public class fragmentAgenda extends Fragment {
-
+    private TextView txtTareas;
+    private TextView txtHabitos;
     private ListView lvTareas;
     private ListView lvHabitos;
+    private Spinner spFiltro;
 
     public fragmentAgenda() {}
 
@@ -34,10 +40,85 @@ public class fragmentAgenda extends Fragment {
         lvTareas = view.findViewById(R.id.lvTareas);
         lvHabitos = view.findViewById(R.id.listViewHabitos);
 
+        txtHabitos= view.findViewById(R.id.txtHabitos);
+        txtTareas= view.findViewById(R.id.txtTareas);
+
+
         mostrarTareas();
         mostrarHabitos();
 
+        // SPINNER FILTRO
+
+        spFiltro = view.findViewById(R.id.spFiltro);
+
+        String[] opciones = {
+                "Todo",
+                "Tareas (todas)",
+                "Tareas hechas",
+                "Tareas pendientes",
+                "Hábitos (todos)",
+                "Hábitos hechos",
+                "Hábitos no hechos"
+        };
+
+        ArrayAdapter<String> adapterSpinner = new ArrayAdapter<>(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                opciones
+        );
+
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spFiltro.setAdapter(adapterSpinner);
+        // 🔥 ACÁ VA EL LISTENER
+        spFiltro.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        mostrarTareas();
+                        mostrarHabitos();
+                        break;
+
+                    case 1:
+                        mostrarTareas(); //Todas las tareas
+                        break;
+
+                    case 2:
+                        mostrarTareasFiltradas(false);// Tareas hechas
+                        break;
+
+                    case 3:
+                        mostrarHabitosFiltrados(true);// Tareas pendientes
+                        break;
+
+                    case 4:
+                        mostrarHabitos(); //Todos los habitos
+                        break;
+
+                    case 5:
+                         // mostrar habitos hechos
+                        break;
+
+                    case 6:
+                        // mostrar habitos no hechos
+                        break;
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        // ⚠️ OPCIONAL: esto ya no hace falta si usás el spinner
+        // mostrarTareas();
+        // mostrarHabitos();
+
         return view;
+
+
     }
 
     private void mostrarTareas() {
@@ -51,6 +132,12 @@ public class fragmentAgenda extends Fragment {
 
         if(listaTareas == null){
             listaTareas = new ArrayList<>();
+        }
+
+
+        if (negocio.contarTareasPendientes() + negocio.contarTareasHechas() == 0){
+            txtTareas.setVisibility(View.VISIBLE);
+            txtTareas.setText("No tenés tareas todavía");
         }
 
         tareaAdapter adapter = new tareaAdapter(requireContext(), listaTareas);
@@ -68,6 +155,10 @@ public class fragmentAgenda extends Fragment {
 
         if (listaHabitos == null) {
             listaHabitos = new ArrayList<>();
+        }
+        if (negocio.contarHabitosNoHechos() + negocio.contarHabitosHechos() == 0){
+            txtHabitos.setVisibility(View.VISIBLE);
+            txtHabitos.setText("No tenés hábitos todavía");
         }
 
         habitoAdapter adapter = new habitoAdapter(requireContext(), listaHabitos);
